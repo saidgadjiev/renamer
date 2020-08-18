102c88e35793a619b1f0e54339b2173e83781558
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-08-19 00:43:49 +0300

    PMD

--- src/main/java/ru/gadjini/telegram/renamer/service/rename/RenameService.java
+++ src/main/java/ru/gadjini/telegram/renamer/service/rename/RenameService.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.gadjini.telegram.renamer.bot.command.bot.SetThumbnailCommand;
 import ru.gadjini.telegram.renamer.bot.command.keyboard.RenameState;
 import ru.gadjini.telegram.renamer.common.CommandNames;
 import ru.gadjini.telegram.renamer.common.MessagesProperties;
