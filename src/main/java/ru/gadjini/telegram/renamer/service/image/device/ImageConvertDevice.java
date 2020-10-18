d209d15982269f8f24e8ddc017a69a4b25ea96a1
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-10-18 22:30:19 +0300

    Fix работы с ProcessExecutor

--- src/main/java/ru/gadjini/telegram/renamer/service/image/device/ImageConvertDevice.java
+++ src/main/java/ru/gadjini/telegram/renamer/service/image/device/ImageConvertDevice.java
@@ -4,17 +4,5 @@ public interface ImageConvertDevice {
 
     void convert(String in, String out, String ... options);
 
-    void negativeTransparent(String in, String out, String inaccuracy, String... colors);
-
-    void positiveTransparent(String in, String out, String inaccuracy, String color);
-
-    void applyBlackAndWhiteFilter(String in, String out);
-
-    void applyNegativeFilter(String in, String out);
-
-    void applySketchFilter(String in, String out);
-
-    void resize(String in, String out, String size);
-
     void convertToThumb(String in, String out);
 }
--- src/main/java/ru/gadjini/telegram/renamer/service/image/device/ImageMagickDevice.java
+++ src/main/java/ru/gadjini/telegram/renamer/service/image/device/ImageMagickDevice.java
@@ -1,5 +1,6 @@
 package ru.gadjini.telegram.renamer.service.image.device;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.gadjini.telegram.smart.bot.commons.service.ProcessExecutor;
 
@@ -10,49 +11,26 @@ import java.util.List;
 @Component
 public class ImageMagickDevice implements ImageConvertDevice, ImageIdentifyDevice {
 
+    private ProcessExecutor processExecutor;
+
+    @Autowired
+    public ImageMagickDevice(ProcessExecutor processExecutor) {
+        this.processExecutor = processExecutor;
+    }
+
     @Override
     public void convert(String in, String out, String... options) {
-        new ProcessExecutor().execute(getCommand(in, out, options));
-    }
-
-    @Override
-    public void negativeTransparent(String in, String out, String inaccuracy, String... colors) {
-        new ProcessExecutor().execute(getTransparentRemoveCommand(in, out, true, inaccuracy, colors));
-    }
-
-    @Override
-    public void positiveTransparent(String in, String out, String inaccuracy, String color) {
-        new ProcessExecutor().execute(getTransparentRemoveCommand(in, out, false, inaccuracy, color));
-    }
-
-    @Override
-    public void applyBlackAndWhiteFilter(String in, String out) {
-        new ProcessExecutor().execute(getBlackAndWhiteFilterCommand(in, out));
-    }
-
-    @Override
-    public void applyNegativeFilter(String in, String out) {
-        new ProcessExecutor().execute(getNegativeFilterCommand(in, out));
-    }
-
-    @Override
-    public void applySketchFilter(String in, String out) {
-        new ProcessExecutor().execute(getSketchFilterCommand(in, out));
-    }
-
-    @Override
-    public void resize(String in, String out, String size) {
-        new ProcessExecutor().execute(getResizeCommand(in, out, size));
+        processExecutor.execute(getCommand(in, out, options));
     }
 
     @Override
     public void convertToThumb(String in, String out) {
-        new ProcessExecutor().execute(getThumbCommand(in, out));
+        processExecutor.execute(getThumbCommand(in, out));
     }
 
     @Override
     public String getSize(String in) {
-        return new ProcessExecutor().executeWithResult(getSizeCommand(in));
+        return processExecutor.executeWithResult(getSizeCommand(in));
     }
 
     private String[] getThumbCommand(String in, String out) {
@@ -70,76 +48,6 @@ public class ImageMagickDevice implements ImageConvertDevice, ImageIdentifyDevic
         command.add("-format");
         command.add("\"%wx%h\"");
         command.add(in);
-
-        return command.toArray(new String[0]);
-    }
-
-    private String[] getResizeCommand(String in, String out, String size) {
-        List<String> command = new ArrayList<>(convertCommandName());
-        command.add(in);
-        command.add("-resize");
-        command.add(size);
-        command.add("-quality");
-        command.add("100");
-        command.add(out);
-
-        return command.toArray(new String[0]);
-    }
-
-    private String[] getNegativeFilterCommand(String in, String out) {
-        List<String> command = new ArrayList<>(convertCommandName());
-        command.add(in);
-        command.add("-channel");
-        command.add("RGB");
-        command.add("-negate");
-        command.add(out);
-
-        return command.toArray(new String[0]);
-    }
-
-    private String[] getBlackAndWhiteFilterCommand(String in, String out) {
-        List<String> command = new ArrayList<>(convertCommandName());
-        command.add(in);
-        command.add("-colorspace");
-        command.add("Gray");
-        command.add(out);
-
-        return command.toArray(new String[0]);
-    }
-
-    private String[] getSketchFilterCommand(String in, String out) {
-        List<String> command = new ArrayList<>(convertCommandName());
-        command.add(in);
-        command.add("(");
-        command.add("-clone");
-        command.add("0");
-        command.add("-negate");
-        command.add("-blur");
-        command.add("0x5");
-        command.add(")");
-        command.add("-compose");
-        command.add("colordodge");
-        command.add("-composite");
-        command.add("-modulate");
-        command.add("100,0,100");
-        command.add("-auto-level");
-        command.add(out);
-
-        return command.toArray(new String[0]);
-    }
-
-    private String[] getTransparentRemoveCommand(String in, String out, boolean negative, String inaccuracy, String... colors) {
-        List<String> command = new ArrayList<>(convertCommandName());
-        command.add(in);
-        command.add("-fuzz");
-        command.add(inaccuracy + "%");
-        String sign = negative ? "-" : "+";
-        for (String color : colors) {
-            command.add(sign + "transparent");
-            command.add(color);
-        }
-
-        command.add(out);
 
         return command.toArray(new String[0]);
     }
